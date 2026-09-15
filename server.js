890eacdfd669fb7b8d364c2d90710208b8f9e649
luamfodasi-cmyk <luamfodasi@gmail.com>
2026-09-15 10:26:30 -0300

    Cria nova tela inicial

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>🌿 CloverSpace</h1>
-    <p>Bem-vindo à nossa nova plataforma!</p>
-  `);
-});
+app.use(express.static("public"));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`CloverSpace rodando na porta ${PORT}`);
